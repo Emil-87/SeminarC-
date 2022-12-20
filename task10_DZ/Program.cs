cf808ff065a8ddf3ef0e2a2454e2a3a748bb0408
Emil-87 <emil.pegas@gmail.com>
2022-12-21 02:09:12 +0600

    task10_DZ

---
+++ task10_DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите три числа");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int a = number / 10;
+    int b = a % 10;
+    Console.WriteLine("Это вторая цифра числа = " + b);
+}
+else
+{
+    Console.WriteLine("Неверный ввод.");
+}
