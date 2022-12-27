21f5aafbf19b787f47c2224ff0a370a554ea3bfd
Emil-87 <emil.pegas@gmail.com>
2022-12-28 01:03:09 +0600

    task019_DZ

---
+++ task019_DZ/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19 ДЗ.
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int number = 23432;
+int palindrom_number = number;
+int result = 0;
+while (palindrom_number > 0)
+{
+    int digit = palindrom_number % 10;
+    result = result * 10 + digit;
+    palindrom_number /= 10;
+    Console.WriteLine(result);
+}
+if (result == number)
+{
+    Console.WriteLine($"Число: {number} палиндром.");
+}
+else
+{
+    Console.WriteLine($"Число: {number} не палиндром.");
+}
