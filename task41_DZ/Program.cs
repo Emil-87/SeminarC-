9eb4d19963a12838b89002f2b8f2623df7cfd477
Emil-87 <emil.pegas@gmail.com>
2023-01-27 23:22:32 +0600

    Task41_DZ

---
+++ task41_DZ/Program.cs
@@ -0,0 +1,17 @@
+﻿void task41_DZ()
+{
+    // Задача 41. Пользователь вводит с клавиатуры М чисел.
+    // Посчитайте, сколько чисел больше 0 ввел пользователь?
+    Console.WriteLine(" Сколько чисел требуется ввести?: ");
+    int countOfnumbers = Convert.ToInt32(Console.ReadLine());
+    int count = 0;
+    for (int i = 1; i <= countOfnumbers; i++)
+    {
+        Console.WriteLine($"Введите {i} число: ");
+        int inputNumber = Convert.ToInt32(Console.ReadLine());
+        if (inputNumber > 0) count++;
+    }
+    Console.WriteLine("Положительных чисел введено: " + count);
+
+}
+task41_DZ();
